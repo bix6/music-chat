516a2d9f557f5ee673fe22f52478bf6b3f22bb2a
bix <840573+bix6@users.noreply.github.com>
2020-07-09 16:57:18 -0700

    update API_ENDPOINT

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export default {
-  API_ENDPOINT: "http://localhost:8000/api",
+  //API_ENDPOINT: "http://localhost:8000/api",
+  API_ENDPOINT: "https://music-chat-bix6.herokuapp.com/api",
   API_KEY: process.env.REACT_APP_API_KEY,
   YOUTUBE_API_KEY: process.env.REACT_APP_YOUTUBE_API_KEY,
 };
