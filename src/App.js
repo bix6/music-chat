90ce3f94abc175b6c7eca62ec6ab1e508a42cece
bix <840573+bix6@users.noreply.github.com>
2020-07-13 18:38:31 -0700

    make socket console log

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import LandingPage from "./LandingPage/LandingPage";
 import ChatPage from "./ChatPage/ChatPage";
+// Setup WebSocket
+import io from "socket.io-client";
+import config from "./config";
+const socket = io.connect(config.API_ENDPOINT);
 
 class App extends React.Component {
   state = {
